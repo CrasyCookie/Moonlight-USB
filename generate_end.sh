ee93bb2988b71e34d7e8f8ddcdfdc13e1f0083bf
CrasyCookie <mail-forward@mail.crasycookie.com>
2024-10-10 21:21:25 +0200

    Added a check to check if aports already exists

--- generate_end.sh
+++ generate_end.sh
@@ -11,10 +11,12 @@ error() {
 # Use [ACTION] for things that have to be done
 printf "[INFO] Generating keys\n"
 abuild-keygen -i -a || printf "[WARN] Failed to generate keys\n"
-printf "[INFO] Cloning aports\n"
-git clone --depth=1 https://gitlab.alpinelinux.org/alpine/aports.git "$BUILD_DIRECTORY"/aports/ || error "failed to clone aports"
+printf "[INFO] Cloning aports, this might take a while\n"
+if [ ! -d "$BUILD_DIRECTORY"/aports ]; then
+    git clone --depth=1 "https://gitlab.alpinelinux.org/alpine/aports.git" "$BUILD_DIRECTORY"/aports/ || error "failed to clone aports"
+fi
 printf "[INFO] Updating apk repositories\n"
-"$PRIVELEGE_PREFIX" apk update || error "Package update failed"
+"$PRIVELEGE_PREFIX" apk update || error "failed to update package repositories"
 mkdir -pv "$BUILD_DIRECTORY"/tmp && export TMPDIR="$BUILD_DIRECTORY"/tmp
 printf "[INFO] Created tempory directory at %s\n" "$TMPDIR"
 "$PRIVELEGE_PREFIX" ln aports_scripts/* "$BUILD_DIRECTORY"/aports/scripts/ || error "Linking scripts failed"
