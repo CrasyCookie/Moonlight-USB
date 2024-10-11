791b6010432284524fa7405d6eb081adafe5ce45
CrasyCookie <mail-forward@mail.crasycookie.com>
2024-10-11 22:13:21 +0200

    Added permissions for the mkimage.sh script
    Signed-off-by: CrasyCookie <mail-forward@mail.crasycookie.com>

--- build_alt.sh
+++ build_alt.sh
@@ -28,7 +28,7 @@ fi
 
 # Run mkimage.sh with all validated parameters
 #"$BUILD_DIRECTORY/aports/scripts/mkimage.sh"
-"$BUILD_DIRECTORY/aports/scripts/mkimage.sh" --tag "$RELEASE" \
+$PRIVILEGE_PREFIX "$BUILD_DIRECTORY/aports/scripts/mkimage.sh" --tag "$RELEASE" \
     --arch "$BUILD_ARCHITECTURE" \
     --profile "$PROFILENAME" \
     --repository "https://$APK_REPOSITORY_MIRROR/alpine/$RELEASE/main" \
