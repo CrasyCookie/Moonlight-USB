c1c16febea5c9b6324458e6439672d9c973902d5
CrasyCookie <mail-forward@mail.crasycookie.com>
2024-10-11 21:40:29 +0200

    Removed the automatic download of aports as a separate repository
    Signed-off-by: CrasyCookie <mail-forward@mail.crasycookie.com>

--- generate_end.sh
+++ generate_end.sh
@@ -11,11 +11,11 @@ error() {
 # Use [ACTION] for things that have to be done
 printf "[INFO] Generating keys\n"
 abuild-keygen -i -a || printf "[WARN] Failed to generate keys\n"
-if [ ! -d "$BUILD_DIRECTORY"/aports ]; then
-    printf "[WARN] A minimal version of aports should be installed, but isn't installed\n"
-    printf "[INFO] Cloning aports, this might take a while\n"
-    git clone --depth=1 "https://gitlab.alpinelinux.org/alpine/aports.git" "$BUILD_DIRECTORY"/aports/ || error "failed to clone aports"
-fi
+#if [ ! -d "$BUILD_DIRECTORY"/aports ]; then
+#    printf "[WARN] A minimal version of aports should be installed, but isn't installed\n"
+#    printf "[INFO] Cloning aports, this might take a while\n"
+#    git clone --depth=1 "https://gitlab.alpinelinux.org/alpine/aports.git" "$BUILD_DIRECTORY"/aports/ || error "failed to clone aports"
+#fi
 printf "[INFO] Updating apk repositories\n"
 apk update || error "failed to update package repositories"
 mkdir -pv "$BUILD_DIRECTORY"/tmp && export TMPDIR="$BUILD_DIRECTORY"/tmp
