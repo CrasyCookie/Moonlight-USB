4343200ce36fa718961e343c8f045b2b62895b7a
CrasyCookie <mail-forward@mail.crasycookie.com>
2024-10-11 21:37:51 +0200

    Fixed  showing /settings.sh in directory
    Signed-off-by: CrasyCookie <mail-forward@mail.crasycookie.com>

--- settings.sh
+++ settings.sh
@@ -4,5 +4,5 @@ PROFILENAME="moonlight_usb" # ISO name
 BUILD_ARCHITECTURE="x86_64" # Supported architectures: x86 x86_64 armhf armv7 aarch64
 RELEASE="edge" # The alpine release to build off
 APK_REPOSITORY_MIRROR="uk.alpinelinux.org" # See mirrors.alpinelinux.org
-BUILD_DIRECTORY="$(realpath $0)"
+BUILD_DIRECTORY="$(dirname "$(realpath $0)")/"
 PRIVILEGE_PREFIX="doas" # doas, sudo, run0 etc
